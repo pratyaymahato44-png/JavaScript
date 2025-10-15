2d90aa337e1e259c206ba792dd51c2fac0f47cf9
pratyaymahato44-png <pratyaymahato44@gmail.com>
2025-10-15 16:30:23 +0000

    mathAndNum

---
+++ Basics/numberAndMath.js
@@ -0,0 +1,27 @@
+const score=400
+//console.log(score)
+
+const balance = new Number(100)
+// console.log(balance)
+// console.log(balance.toFixed(2))
+
+const otherNum=1623.124335
+// console.log(otherNum.toPrecision(3))
+
+const hundread=10000000
+//console.log(hundread.toLocaleString("en-IN"))
+// ****************** Math ************************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(24.5))
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.9))
+// console.log(Math.min(2,5,7,9))
+
+console.log(Math.random())
+//console.log((Math.random()*10)+1)
+console.log(Math.floor((Math.random()*10)) + 1)
+const max=20
+const min=10
+console.log(Math.floor(Math.random()*(max-min + 1))+min)
